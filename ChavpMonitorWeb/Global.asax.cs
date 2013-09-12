501e194c6ee1604b7c140c4d73eade8f8d7bb553
chavp <my.parinya@gmail.com>
2013-09-12 08:23:44 +0700

    fix cpuUsages size
    Signed-off-by: chavp <my.parinya@gmail.com>

--- ChavpMonitorWeb/Global.asax.cs
+++ ChavpMonitorWeb/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using Chavp.Monitors;
-using EasyNetQ;
-using Microsoft.AspNet.SignalR;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,9 +7,13 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
 
 namespace ChavpMonitorWeb
 {
+    using Chavp.Monitors;
+    using EasyNetQ;
+
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
     // http://www.asp.net/signalr
